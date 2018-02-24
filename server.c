015e162c8ca80a1bffbfb694f315eca7710d2037
laur <duicul@yahoo.com>
2018-02-24 04:07:39 +0200

    clients

--- server.c
+++ server.c
@@ -12,6 +12,48 @@
 #include <netdb.h>
 #include <errno.h>
 #define BUFF_SIZE 200
+struct client{
+struct sockaddr_in *h;
+struct client *urm;
+}*rad=NULL;
+
+void add_client(struct sockaddr_in *p)
+{struct client *o=malloc(sizeof(struct client));
+struct sockaddr_in *q=malloc(sizeof(struct sockaddr_in));
+*q=*p;
+o->h=q;
+o->urm=NULL;
+if(rad==NULL)
+rad=o;
+else{ 
+o->urm=rad->urm;
+rad=o;}}
+
+void remove_client(struct sockaddr_in *p)
+{struct client *i,*aux,*q;
+for(i=rad,q=NULL;i!=NULL&&(i->h->sin_addr.s_addr)!=(p->sin_addr.s_addr);q=i,i=i->urm);
+if(q==NULL)
+{aux=rad;
+rad=rad->urm;
+free(aux->h);
+free(aux);}
+else {aux=i;q->urm=i->urm;free(aux->h);
+free(aux);}}
+
+int check_client(struct sockaddr_in *p)
+{struct client *i,*aux,*q;
+for(i=rad,q=NULL;i!=NULL&&(i->h->sin_addr.s_addr)!=(p->sin_addr.s_addr);q=i,i=i->urm);
+if(i==NULL)
+return -1;
+return 0;}
+
+void list_client()
+{struct client *i;
+printf("Client list \n");
+for(i=rad;i!=NULL;i=i->urm)
+printf("%d\n",i->h->sin_addr.s_addr);
+}
+
 int setaddr(struct sockaddr_in *addr,char inaddr[],u_int32_t a,short sinport) {
 struct hostent *h;
 memset((void*)addr,0,sizeof(addr));
@@ -60,7 +102,9 @@ if (listen(sockfd,10) < 0)
 printf("3%s\n",strerror(errno));
 for (;;)
 {newsockfd=accept(sockfd,(struct sockaddr *)&rem,&rlen);/* blocare pentru asteptare cereri*/
- printf("4%s\n",strerror(errno)); 
+ if(check_client(&rem)==-1)
+   add_client(&rem); 
+printf("4%s\n",strerror(errno)); 
 if (newsockfd < 0) 
    printf ("error4 ...\n"); 
   else
@@ -68,8 +112,10 @@ if (newsockfd < 0)
   if(fork()==0)
   {readline(newsockfd,line); /*deservire cerere*/
   printf("line: %s",line);
-if(strcmp(line,"~exit\n")!=0)
- printf("Finish\n");
+  write(newsockfd,line,strlen(line));
+  list_client();
+  if(strcmp(line,"~exit\n")==0)
+  {remove_client(&rem);printf("Finish\n");list_client();}
   close (newsockfd);  
 }}
 }
